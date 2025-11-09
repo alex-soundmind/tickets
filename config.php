2b3edf233452ff7e2342be6f04c7c5332df37883
alex-soundmind <146514354+alex-soundmind@users.noreply.github.com>
2025-11-09 11:06:07 +0300

    Update config.php

--- config.php
+++ config.php
@@ -3,6 +3,7 @@ $db   = 'tickets';
 $host = 'dpg-d4847mbipnbc73d8hlm0-a.singapore-postgres.render.com';
 $user = 'user';
 $pass = 'RFa4bfOpswyRFBK3cZvaU9okEORsFxYO';
+$dsn = "pgsql:host=$host;dbname=$db";
 
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
